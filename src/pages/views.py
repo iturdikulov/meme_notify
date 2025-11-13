423fc476e0cae132a756f3f511043ca2eeebb169
Inom T. <i@inomoz.ru>
2025-11-13 11:03:02 +0300

    Integrate basic form to generate notification

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,13 +1,27 @@
-import os
-
-from django import get_version
 from django.conf import settings
+from django.contrib import messages
+from django.forms import modelformset_factory
 from django.shortcuts import render
+from django.shortcuts import redirect
+
+from pages.models import NotificationGenerator
 
 
 def home(request):
-    context = {
-        "debug": settings.DEBUG,
-    }
+    NotificationGeneratorFormSet = modelformset_factory(
+        NotificationGenerator, fields=["channel", "notifications_amount"]
+    )
+
+    if request.method == "POST":
+        formset = NotificationGeneratorFormSet(request.POST or None, request.FILES)
+        if formset.is_valid():
+            formset.save()
+            messages.add_message(
+                request, messages.INFO, "Notifications generated successfully."
+            )
+    else:
+        formset = NotificationGeneratorFormSet()
+
+    context = {"debug": settings.DEBUG, "formset": formset}
 
     return render(request, "pages/home.html", context)
